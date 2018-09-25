6e84e5754adea3784fc814304397f9e6d0ea2bfd
Daniel Balparda <balparda@google.com>
2018-09-25 14:25:16 +0100

    GPLing this code

--- irish_rail.py
+++ irish_rail.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3 -O
 #
-# Copyright 2018 Daniel Balparda (balparda@gmail.com)
+# Copyright (C) 2018 Daniel Balparda de Carvalho (balparda@gmail.com).
+# This file is part of Irish Rail Timetable.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/gpl-3.0.txt.
 #
 """Irish Rail data converter."""
 
@@ -310,7 +323,11 @@ def tables(
   # set logging level
   logging.basicConfig(
       level=_VERBOSITY_LEVELS[verbosity_level if verbosity_level <= 2 else 2], format=_LOG_FORMAT)
-  logging.info('START: IRISH RAIL CUSTOM TIMETABLE')
+  logging.info('IRISH RAIL TIMETABLE')
+  logging.info('Copyright (C) 2018 Daniel Balparda de Carvalho (balparda@gmail.com)')
+  logging.info('This program comes with ABSOLUTELY NO WARRANTY; '
+               'this is free software, and you are welcome to redistribute it under certain '
+               'conditions; see LICENSE file for details.')
   # load oficial routes and stops first, as we might need to print those
   routes, stops_names = _LoadRoutes(), _LoadStops()
   if operation == 'list':
